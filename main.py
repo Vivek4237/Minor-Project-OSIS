63b9ef2d7f596a87959291307df36812db9e9b3a
Vivek Sahani <99320742+Vivek4237@users.noreply.github.com>
2022-05-18 15:49:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,130 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+import pyaudio
+from selenium import webdriver
+from getpass import getpass
+
+print("Initializing OSIS")
+MASTER = "Vivek"
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+#Speak function will speak/Pronounce the string which is passed to it
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+#This funtion will wish you as per the current time
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    print(hour)
+
+    if hour>=0 and hour <12:
+        speak("good morning" + MASTER)
+
+    elif hour>=12 and hour<18:
+        speak("good afternoon" + MASTER)
+
+    else:
+        speak("good Evening" + MASTER)
+
+    speak("i am your assistant. How may I help you?")
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('User-email-id', 'Password')
+    server.sendmail("From","to","msg")
+    server.close()
+
+    
+
+
+
+#This function will take command from the microphone
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    try :
+        print("Recognizing...")
+        query = r.recognize_google(audio, language = 'en-in')
+        print(f"user said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        query = None
+
+    return query
+
+#main program starting
+def main():
+    speak("Initializing OSIS...")
+    wishMe()
+    query = takeCommand()
+
+    #Logic for executing tasks as per the query
+    if 'wikipedia' in query.lower():
+        speak('searching wikipedia...')
+        query = query.replace("wikipedia", "")
+        results = wikipedia.summary(query, sentences =2)
+        print(results)
+        speak(results)
+
+    elif 'open youtube' in query.lower():
+        #webbrowser.open('youtube.com')
+        url = "youtube.com"
+        chrome_path = 'c:/program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open(url)
+
+    elif 'open google' in query.lower():
+        #webbrowser.open('youtube.com')
+        url = "google.com"
+        chrome_path = 'c:/program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open(url)
+
+    elif 'blackboard' in query.lower():
+        url = "cuchd.blackboard.com"
+        chrome_path = 'c:/program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open(url)
+
+
+    elif 'play music' in query.lower():
+        songs_dir = "C:\\Users\\Legion\\Desktop\\audio"
+        songs = os.listdir(songs_dir)
+        print(songs)
+        os.startfile(os.path.join(songs_dir, songs[0]))
+
+    elif 'time' in query.lower():
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"{MASTER} the time is {strTime}")
+        print(strTime)
+
+    elif 'open code' in query.lower():
+        codePath = "C:\\Users\\LEGION\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+        os.startfile(codePath)
+
+    elif 'open steam' in query.lower():
+        codePath = "C:\\Program Files (x86)\\Steam\\steam.exe"
+        os.startfile(codePath)
+            
+    elif 'send message' in query.lower():
+        try:
+            speak("what should i send")
+            content = takeCommand()
+            to = "sainibhanu96@gmail.com"
+            sendEmail(to, content)
+            speak("Email has been sent to bhanu")
+        except Exception as e:
+            print(e)
+
+main()
